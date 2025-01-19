931a7d311108530de76c09700a86c4fe7d221d35
Kazi Solayman <kazisolayman123@gmail.com>
2025-01-19 16:10:33 +0600

    connected cloudinary

--- server.js
+++ server.js
@@ -2,12 +2,14 @@ import express from "express";
 import cors from "cors";
 import connectDB from "./config/mongodb.js";
 import { configDotenv } from "dotenv";
+import connectCloudinary from "./config/cloudinary.js";
 configDotenv();
 
 // server config
 const app = express();
 const PORT = 8000;
 connectDB();
+connectCloudinary();
 
 // middleware
 app.use(cors());
