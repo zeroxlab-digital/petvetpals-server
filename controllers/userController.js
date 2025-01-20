bcee42098668d3ab0ec5f4334206c0e632d2a05f
Kazi Solayman <kazisolayman123@gmail.com>
2025-01-20 20:52:19 +0600

    user login functionalities added

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,3 +1,4 @@
+import jwt from "jsonwebtoken";
 import { User } from "../models/userModel.js";
 import bcrypt from "bcrypt";
 
@@ -21,6 +22,28 @@ export const userRegister = async (req, res) => {
             password: dashedPassword
         })
         res.status(200).json({ status: "success", message: "User created successfully!" });
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+export const userLogin = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        if (!email || !password) {
+            return res.status(400).json({ message: "All fields are required!" });
+        }
+        const user = await User.findOne({ email });
+        if (!user) {
+            return res.status(404).json({ message: "User not found!" });
+        }
+        const matchedPassword = await bcrypt.compare(password, user.password);
+        if (!matchedPassword) {
+            return res.status(400).json({ message: "Invalid credentials!" });
+        }
+        const userDetails = await User.findOne({ email }).select("-password");
+        const user_token = await jwt.sign({ userData: user._id }, process.env.JWT_SECRET_KEY, { expiresIn: '1d' });
+        res.status(200).cookie("user_token", user_token, { maxAge: 1 * 24 * 60 * 60 * 1000, httpOnly: true, sameSite: 'strict' }).json({ success: 'true', message: "User login successfull!", userDetails })
     } catch (error) {
         console.log(error);
     }
--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,7 +1,8 @@
 import express from "express";
-import { userRegister } from "../controllers/userController.js";
+import { userLogin, userRegister } from "../controllers/userController.js";
 const userRouter = express.Router();
 
 userRouter.post("/register", userRegister);
+userRouter.post("/login", userLogin);
 
 export default userRouter;
