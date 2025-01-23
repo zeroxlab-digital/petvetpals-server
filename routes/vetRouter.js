e18bb9a8aea6952ede9bd075e870e348b75ec0a4
Kazi Solayman <kazisolayman123@gmail.com>
2025-01-23 19:40:32 +0600

    removed commented unused code

--- routes/vetRouter.js
+++ routes/vetRouter.js
@@ -2,10 +2,6 @@ import express from "express";
 import { getAllVets, getVet, loginVet, registerVet } from "../controllers/vetController.js";
 const vetRouter = express.Router();
 
-// Vet appointment routers
-// vetRouter.get("/view-appointments", userAuthenticated, viewAppointments);
-// vetRouter.post("/:id/appointment", userAuthenticated, bookAppointment);
-
 // vet registration and login routers
 vetRouter.post("/register", registerVet);
 vetRouter.post("/login", loginVet);
