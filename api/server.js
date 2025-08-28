5d2e9924170383c002d3cb06632107bf87262b21
Kazi Solayman <kazisolayman123@gmail.com>
2025-08-28 16:03:51 -0500

    Added main domain on cors options allow origin

--- api/server.js
+++ api/server.js
@@ -32,7 +32,7 @@ app.use(express.json());
 app.use(cookieParser());
 const corsOptions = {
     origin: (origin, callback) => {
-        const allowedOrigins = ['http://localhost:3000', 'https://petvetpals.vercel.app'];
+        const allowedOrigins = ['http://localhost:3000', 'https://petvetpals.vercel.app', 'https://www.petvetpals.com'];
         if (!origin || allowedOrigins.includes(origin)) {
             callback(null, true);
         } else {
