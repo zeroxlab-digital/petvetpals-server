965c658b988b7c7b8a229dd51b15ccc47e999214
Kazi Solayman <kazisolayman123@gmail.com>
2026-08-07 03:59:43 -0500

    Migrated mongodb, cloudinary configs and multer middleware

--- src/config/mongodb.ts
+++ src/config/mongodb.ts
@@ -3,15 +3,15 @@ import mongoose from "mongoose";
 const connectDB = async () => {
     try {
         await mongoose.connect(`${process.env.MONGODB_URI}/PetVetPals`, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
             serverSelectionTimeoutMS: 5000, // Wait max 5s for DB response
-            maxPoolSize: 10, // Increase the connection pool size
+            maxPoolSize: 10, // connection pool size
         });
         console.log("✅ Database connected successfully!");
-    } catch (error) {
-        console.error("❌ MongoDB Connection Error:", error.message);
-        process.exit(1);
+    } catch (error: unknown) {
+        if (error instanceof Error) {
+            console.error("❌ MongoDB Connection Error:", error.message);
+            process.exit(1);
+        }
     }
 };
 
